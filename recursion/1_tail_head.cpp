af114b46c28ec5b8b1e1b1683e7d118b43aad964
Sanju <sanju.mca19.du@gmail.com>
2020-12-28 00:00:02 +0530

    Add files via upload
    head vs tail recursion.

---
+++ recursion/1_tail_head.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+void fun_tail(int n)          //Tail Recursion
+{
+    if(n>0)
+    {
+        cout<<n<<" ";
+        fun_tail(n-1);
+    }
+}
+
+void fun_head(int n)          //Head Recursion
+{
+    if(n>0)
+    {
+        fun_head(n-1);
+        cout<<n<<" ";
+        
+    }
+}
+
+int main()
+{
+    int x;
+    cout<<"Enter n : ";  
+    cin>>x;
+    fun_tail(x);
+    cout<<endl;
+    fun_head(x);
+    
+    return 0;
+}
