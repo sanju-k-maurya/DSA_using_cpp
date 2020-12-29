99aa29d0fb63606f9728cad2966ab8ea1471ac59
Sanju <sanju.mca19.du@gmail.com>
2020-12-30 00:56:53 +0530

    Update 5_nested_recursion.cpp

--- recursion/5_nested_recursion.cpp
+++ recursion/5_nested_recursion.cpp
@@ -5,7 +5,7 @@ int fun(int n)
 {
     if(n>100)
         return n-10;  
-    return fun(fun(n+11));     //Nested Recursion - recursive call paremeter is funcion.
+    return fun(fun(n+11));     //Nested Recursion - recursive call paremeter is function itself.
 }
 int main()
 {
