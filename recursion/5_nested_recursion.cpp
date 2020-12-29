1de7d044a9aa7f5cf55609e11398fec6bab61b97
Sanju <sanju.mca19.du@gmail.com>
2020-12-30 00:55:24 +0530

    Add files via upload

---
+++ recursion/5_nested_recursion.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int fun(int n)
+{
+    if(n>100)
+        return n-10;  
+    return fun(fun(n+11));     //Nested Recursion - recursive call paremeter is funcion.
+}
+int main()
+{
+    int x;
+    cout<<"Enter x : ";
+    cin>>x;
+    int r = fun(x);
+    cout<<r<<endl;
+
+    return 0;
+}
